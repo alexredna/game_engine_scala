c27e71c70241ebc94826888535460548b5cf26f4
Matt Allen <matt_allen@utexas.edu>
2014-12-01 14:15:14 -0600

    Updated error message sample

--- ErrorMessage.scala
+++ ErrorMessage.scala
@@ -1,18 +1,16 @@
 object ErrorMessage extends JazzFramework {
-	def main(args: Array[String]) {
-		ScalaFrame vertical split
-		ScalaFrame add (Create label 'message text "You fail at life.")
-		ScalaFrame add (Create hPanel 'buttons)
-		'buttons add (Create button 'yes text "I'm sorry" onClick yes)
-		'buttons add (Create button 'no text "I disagree" onClick no)
-		Run
-	}
+  def main(args: Array[String]) {
+	ScalaFrame vertical split
+	ScalaFrame add (Create label 'message text "You fail at life.")
+	ScalaFrame add (Create hPanel 'buttons having
+	  an add (Create button 'yes having
+        a text "I'm sorry" and
+        an onClick yes) and
+	  an add (Create button 'no having
+        a text "I disagree" and
+        an onClick (() => 'message text "Dissagree all you want")))
+	Run
+  }
 
-	def yes() {
-		System.exit(0)
-	}
-
-	def no() {
-		'message text "Disagree all you want."
-	}
+  def yes() { System.exit(0) }
 }
